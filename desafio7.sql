f95e5790b41aca7cf09a17fabe7da699a6afd0c5
Sass <dudu_sass12@hotmail.com>
2021-10-05 10:45:06 -0300

    requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+CREATE VIEW perfil_artistas AS
+SELECT 
+a.name AS artista,
+ab.name AS album,
+COUNT(f.artist_id) AS seguidores
+FROM SpotifyClone.artists AS a
+JOIN SpotifyClone.albuns AS a1 ON a.artist_id = a1.artist_id
+JOIN SpotifyClone.follower_artist f ON a.artist_id = f.artist_id
+GROUP BY `album`, `artista`
+ORDER BY `seguidores` DESC, `artista`;
