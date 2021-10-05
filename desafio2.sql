98c7b77505efc6a954b535536bd878f71e6708f0
Sass <dudu_sass12@hotmail.com>
2021-10-05 12:03:59 -0300

    trocando os albums por albuns,confudo demais ingles e portugues mixado

--- desafio2.sql
+++ desafio2.sql
@@ -2,4 +2,4 @@ CREATE VIEW estatisticas_musicais AS
 SELECT 
 (SELECT COUNT(*) FROM SpotifyClone.musics) cancoes,
 (SELECT COUNT(*) FROM SpotifyClone.artists) artistas,
-(SELECT COUNT(*) FROM SpotifyClone.albums) albuns;
+(SELECT COUNT(*) FROM SpotifyClone.albuns) albuns;
